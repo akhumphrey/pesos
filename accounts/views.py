c9a4216f4082a2f308c63e17bd7731fab0e52a8a
Adrian Humphrey <github@ember.co.nz>
2016-09-27 17:53:05 +0100

    corrected account template location

--- accounts/views.py
+++ accounts/views.py
@@ -6,8 +6,8 @@ from .models import Account
 def index(request):
     all_accounts = Account.objects.order_by('name')
     context = {'all_accounts': all_accounts}
-    return render(request, 'index.html', context)
+    return render(request, 'accounts/index.html', context)
 
 def detail(request, account_id):
     account = get_object_or_404(Account, pk=account_id)
-    return render(request, 'detail.html', {'account': account})
+    return render(request, 'accounts/detail.html', {'account': account})
