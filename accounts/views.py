7a1c8b49e251fa438e69f6cbf6395893cdd0cdde
Adrian Humphrey <github@ember.co.nz>
2016-09-27 15:36:00 +0100

    added detail view, added template output

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,13 @@
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Account
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the accounts index.")
+    all_accounts = Account.objects.order_by('name')
+    context = {'all_accounts': all_accounts}
+    return render(request, 'index.html', context)
+
+def detail(request, account_id):
+    account = get_object_or_404(Account, pk=account_id)
+    return render(request, 'detail.html', {'account': account})
