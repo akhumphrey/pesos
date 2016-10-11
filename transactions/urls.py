e71742b6596936bebab5355fae06a1109775f22f
Adrian Humphrey <github@ember.co.nz>
2016-10-11 21:40:49 +0100

    added basic form to transactions index

--- transactions/urls.py
+++ transactions/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'transactions'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^create/$', views.create, name='create'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.views import generic
 
 from .models import Transaction
+from accounts.models import Account
+from envelopes.models import Envelope
 
 class IndexView(generic.ListView):
     template_name = 'transactions/index.html'
@@ -11,3 +13,21 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Transaction.objects.order_by('-date')
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['all_accounts'] = Account.objects.order_by('name')
+        context['all_envelopes'] = Envelope.objects.order_by('name')
+        return context
+
+def create(request):
+    account = get_object_or_404(Account, pk=request.POST['account_id'])
+    envelope = get_object_or_404(Envelope, pk=request.POST['envelope_id'])
+    try:
+        transaction = Transaction(account=account, envelope=envelope, date=request.POST['date'], amount=request.POST['amount'])
+    except (KeyError):
+        return render(request, 'transactions/index.html', {'message': 'Something squiffy happened.'})
+    else:
+        transaction.save()
+        return HttpResponseRedirect(reverse('transactions:index'))
+    return True
